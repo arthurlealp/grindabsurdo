73e660e60e609f8cf9e04875266228a8dadcb09b
Arthur Leal <arthurlealp21@gmail.com>
2026-04-28 11:10:02 -0300

    add backend

---
+++ voke/apresentacao-backend/src/main/java/br/voke/VokeApplication.java
@@ -0,0 +1,12 @@
+package br.voke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VokeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VokeApplication.class, args);
+    }
+}
